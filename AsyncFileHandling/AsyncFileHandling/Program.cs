28b59361d623d959e60730d7de48acd745069189
CodingArc4 <kmunib10@outlook.com>
2023-09-11 22:21:00 +0500

    first commit

---
+++ AsyncFileHandling/AsyncFileHandling/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+
+namespace AsyncFileHandling
+{
+    internal class Program
+    {
+        //public static string Content { get; set; }
+        static async Task Main(string[] args)
+        {
+            string path = @"C:\Users\iT LOGIX\Desktop\FileHandling.txt"; //for reading
+            string path2 = @"C:\Users\iT LOGIX\Desktop\random.txt"; // for writing
+
+
+            var content = await ReadTextFromFile(path);
+            WriteTextToFile(content,path2);
+            Console.ReadLine();
+        }
+
+        static async Task WriteTextToFile(string content,string path)
+        {
+            if (File.Exists(path))
+            {
+                Console.WriteLine("Writing text to file plz wait...");
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    await sw.WriteLineAsync(content);
+                }
+                Console.WriteLine("Writing to file completed!");
+            }
+        }
+
+        static async Task<string?> ReadTextFromFile(string path)
+        {
+            Console.WriteLine("Now reading file...");
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string content="";
+
+                if (sr.ReadLine() != null)
+                {
+                    content =  await sr.ReadToEndAsync();
+                }
+                Console.WriteLine("Reading file completed...");
+                return content;
+            }
+        }
+    }
+}
---
+++ AsynchronusDemo2/AsynchronusDemo2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace AsynchronusDemo2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //call async method
+            Console.WriteLine("--Async Method Call");
+            var myresul2 = await AsyncMethod(50);
+
+            //call async method
+            Console.WriteLine("--Async2 Method Call");
+            var myresul1 = await AsyncMethod2(5);
+            
+            Console.WriteLine($"async2 method call result: {myresul1}");
+            Console.WriteLine($"Async method call result: {myresul2}");
+
+        }
+
+        //asynchronus method
+        private static async Task<int> AsyncMethod(int count)
+        {
+            int result = 0;
+
+            for (int i = 0; i < count; i++)
+            {               
+                Console.WriteLine($"async number print : {i}");
+                result += i;
+            }
+            return result;
+        }
+
+        // Asynchronus method 2
+        private static async Task<int> AsyncMethod2(int count)
+        {
+            int result = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+
+                Console.WriteLine($"async2 number print : {i}");
+                result += i;
+            }
+            return result;
+        }
+
+    }
+}
